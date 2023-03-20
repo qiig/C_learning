a9c96ff5195952abea6e7007d794840c930327e4
host <ki1633@outlook.com>
2023-03-20 22:41:22 +0800

    s.c

--- progs/s.c
+++ progs/s.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
-// 位运算
+
+int f(int n);
+
 int main() 
 {
     int n;
     while (scanf("%d", &n) != EOF) 
     {
-        int count = 0;
-        while (n) 
-        {
-            n = n & (n - 1); //求一个二进制数有几个1，用n与n-1作&；
-            n = n & (n << 1); //求一个二进制数有几个连续的1，用n与n<<1作&
-            count++;
-        }
-        printf("%d\n", count);
+        printf("%d\n", f(n));
     }
     return 0;
+}
+
+int f(int n) {
+    if (n % 2 == 0) {
+        return 2 * n + 3 * n * (n - 1) / 2;
+    } else {
+        return 3 * n - 1 + f(n - 1);
+    }
 }
