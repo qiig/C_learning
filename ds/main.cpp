ce84dbcafd58a7c01a297f0b9a3cb06e6b91bebc
host <ki1633@outlook.com>
2023-04-08 20:23:00 +0800

    update

--- ds/main.cpp
+++ ds/main.cpp
@@ -1,17 +1,36 @@
-#include "ALGraph.h"
+#include "MGraph.h"
 // main
 int main(){
+    int vn, en; // num. of vertices & edges
+    cin >> vn >> en;
     char ch[] = {'A', 'B', 'C', 'D', 'E', 'F'};
-    ALGraph<char> ALG{ch, 6, 6};
+    MGraph<char> MG{ch, vn, en};
     for(int i = 0; i < MaxS; i++){
         visited[i] = 0;
     }
     cout << "depth first traverse: ";
-    ALG.DFTraverse(0);
+    MG.DFTraverse(0);
     for(int i = 0; i < MaxS; i++){
         visited[i] = 0;
     }
     cout << endl << "breadth first traverse: ";
-    ALG.BFTraverse(0);
+    try{
+        MG.BFTraverse(0);
+    } catch(const char* str){cout << str << endl;}
+    cout << endl << "prim: " << endl;
+    MG.Prim(0);
     return 0;
 }
+/*
+6 9
+0 1 34
+0 2 46
+0 5 19
+1 4 12
+2 3 17
+2 5 25
+3 4 38
+3 5 25
+4 5 26
+*/
+// 6 6 0 1 1 0 2 1 0 5 1 1 2 1 1 4 1 2 3 1
