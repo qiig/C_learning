221926dda28c22c019024c56b2642433750d1356
host <ki1633@outlook.com>
2023-05-17 10:19:43 +0800

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,32 @@
+/*
+    binary search
+
+*/
+#include<iostream>
+using namespace std;
+
+int biSearch(int k, int low, int high, int data[])
+{
+    int mid;
+
+    while(low <= high)
+    {
+        mid = (low+high)/2;
+        if(k < data[mid]) high = mid=1;
+        else if(k > data[mid]) low = mid+1;
+        else return mid;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    int data[] = {1, 3, 5, 7, 8, 9, 11, 12, 13};
+    
+    cout << biSearch(8, 0, 8, data) << endl;
+
+    cout << biSearch(10, 0, 8, data) << endl;
+
+    return 0;
+}
